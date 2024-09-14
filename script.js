c9043d348270ca115f6feb38abb931ec96740eca
Jorge Tricarico <jorge.tricarico@gmail.com>
2024-09-14 14:33:10 -0300

    change price list 13-09-24

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const dollarPriceElement = document.getElementById("dollarPrice");
 
   //IMPORTANTE CAMBIAR SI SE SUBE UN NUEVO JSON ↓
-  const currentJsonFileName = "ListaPrecio_03-09-24_json_compres.gz";
+  const currentJsonFileName = "ListaPrecio_13-09-24_json_compres.gz";
   //IMPORTANTE CAMBIAR SI SE SUBE UN NUEVO JSON ↑
 
   let searchTimeout;
